335a7d770c2114f57d63736800c5d7243f8c3a27
David Blyth <dblyth@anl.gov>
2018-10-05 15:16:53 -0500

    CI: fixed the way protos are compiled for consistent FileDescriptorProto names

--- gen.sh
+++ gen.sh
@@ -16,13 +16,14 @@ for proto in $(find proto -iname "*.proto"); do
         echo "option go_package = \"$go_package\";" >> $proto
     fi
     protoc \
-        --proto_path=proio/proto=proto \
         --proto_path=proio/model=proto/model \
+        --proto_path=proio/proto=proto \
         --gofast_out=tmp $proto
 done
 
 # Move code to repo
 mv tmp/proio/proto/* go-proio-pb/
+mv tmp/proio/model go-proio-pb/
 
 # Initialize go module
 cd go-proio-pb
